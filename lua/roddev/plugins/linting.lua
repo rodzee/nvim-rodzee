5c5d3cc3a8d5ae6db3dd036379a7cdb0e9562b56
rodzee <souzafeb@gmail.com>
2024-04-17 12:30:02 -0400

    updates

--- lua/roddev/plugins/linting.lua
+++ lua/roddev/plugins/linting.lua
@@ -20,6 +20,23 @@ return {
 			end,
 		})
 
+		-------------------------------------------------
+
+		local eslint = lint.linters.eslint_d
+
+		eslint.args = {
+			"--no-warn-ignored",
+			"--format",
+			"json",
+			"--stdin",
+			"--stdin-filename",
+			function()
+				return vim.api.nvim_buf_get_name(0)
+			end,
+		}
+
+		-------------------------------------------------
+
 		vim.keymap.set("n", "<leader>l", function()
 			lint.try_lint()
 		end, { desc = "Trigger linting for current file" })
