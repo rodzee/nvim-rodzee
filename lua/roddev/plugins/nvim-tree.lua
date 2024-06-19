fb43847164e492f6158f1971f7d16fed978ba772
rodzee <souzafeb@gmail.com>
2024-06-19 11:07:41 -0400

    fix trouble not working and updates

--- lua/roddev/plugins/nvim-tree.lua
+++ lua/roddev/plugins/nvim-tree.lua
@@ -1,12 +1,29 @@
 return {
 	"nvim-tree/nvim-tree.lua",
 	dependencies = "nvim-tree/nvim-web-devicons",
+	lazy = false,
 	config = function()
 		local nvimtree = require("nvim-tree")
 
 		vim.g.loaded_netrw = 1
 		vim.g.loaded_netrwPlugin = 1
 
+		local function open_nvim_tree(data)
+			-- buffer is a directory
+			local directory = vim.fn.isdirectory(data.file) == 1
+
+			if not directory then
+				return
+			end
+
+			-- change to the directory
+			vim.cmd.cd(data.file)
+
+			-- open the tree
+			require("nvim-tree.api").tree.open()
+		end
+
+		vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 		nvimtree.setup({
 			view = {
 				width = 35,
