5ee4ea2c6e6f3bfeb3e4b6ec2884ba83e429a18c
rodzee <souzafeb@gmail.com>
2024-08-01 09:20:20 -0400

    updates again :)

--- lua/roddev/plugins/colorscheme.lua
+++ lua/roddev/plugins/colorscheme.lua
@@ -1,39 +1,4 @@
 return {
-	-- 	{
-	-- 		"diegoulloao/neofusion.nvim",
-	-- 		name = "neofusion",
-	-- 		priority = 1000,
-	-- 		config = function()
-	-- 			-- Default options:
-	-- 			require("neofusion").setup({
-	-- 				-- terminal_colors = true, -- add neovim terminal colors
-	-- 				undercurl = true,
-	-- 				underline = true,
-	-- 				bold = true,
-	-- 				italic = {
-	-- 					strings = true,
-	-- 					emphasis = true,
-	-- 					comments = true,
-	-- 					operators = false,
-	-- 					folds = true,
-	-- 				},
-	-- 				strikethrough = true,
-	-- 				invert_selection = false,
-	-- 				invert_signs = false,
-	-- 				invert_tabline = false,
-	-- 				invert_intend_guides = false,
-	-- 				inverse = true, -- invert background for search, diffs, statuslines and errors
-	-- 				palette_overrides = {},
-	-- 				overrides = {
-	-- 					["@comment.lua"] = { fg = "#5b5b5b" },
-	-- 					hls = { bg = "#5b5b5b" },
-	-- 				},
-	-- 				dim_inactive = false,
-	-- 				transparent_mode = false,
-	-- 			})
-	-- 			vim.cmd([[ colorscheme neofusion ]])
-	-- 		end,
-	-- 	},
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
